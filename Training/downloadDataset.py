a4483f244ac08b19f751217014d1344dfa559b9a
TWCurry <thomas.w.curry@accenture.com>
2020-12-18 17:29:36 +0000

    Updated s3 download script

--- Training/downloadDataset.py
+++ Training/downloadDataset.py
@@ -1,19 +1,19 @@
 import boto3
 import os
 
-def downloadDirectoryFroms3(bucketName, remoteDirectoryName):
+def downloadS3Directory(bucketName, remoteDirectoryName):
     s3_resource = boto3.resource('s3')
     bucket = s3_resource.Bucket(bucketName)
     for obj in bucket.objects.filter(Prefix = remoteDirectoryName):
-if not os.path.exists(os.path.dirname(obj.key)):
-os.makedirs(os.path.dirname(obj.key))
-print("\rDownloading" + obj.key, end="")
-bucket.download_file(obj.key, obj.key) # save to same path
+        if not os.path.exists(os.path.dirname(obj.key)):
+            os.makedirs(os.path.dirname(obj.key))
+        print("\rDownloading" + obj.key, end="")
+        bucket.download_file(obj.key, obj.key) # save to same path
 
-downloadDirectoryFroms3("twcurry-kdef-dataset-sample", "AF")
-downloadDirectoryFroms3("twcurry-kdef-dataset-sample", "AN")
-downloadDirectoryFroms3("twcurry-kdef-dataset-sample", "DI")
-downloadDirectoryFroms3("twcurry-kdef-dataset-sample", "HA")
-downloadDirectoryFroms3("twcurry-kdef-dataset-sample", "NE")
-downloadDirectoryFroms3("twcurry-kdef-dataset-sample", "SA")
-downloadDirectoryFroms3("twcurry-kdef-dataset-sample", "SU")
+downloadS3Directory("twcurry-kdef-dataset-sample", "AF")
+downloadS3Directory("twcurry-kdef-dataset-sample", "AN")
+downloadS3Directory("twcurry-kdef-dataset-sample", "DI")
+downloadS3Directory("twcurry-kdef-dataset-sample", "HA")
+downloadS3Directory("twcurry-kdef-dataset-sample", "NE")
+downloadS3Directory("twcurry-kdef-dataset-sample", "SA")
+downloadS3Directory("twcurry-kdef-dataset-sample", "SU")
