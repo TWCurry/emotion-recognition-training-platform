99e65de696916f5f56afebb1d09762465c4b31f3
TWCurry <thomas.w.curry@accenture.com>
2020-12-28 11:10:43 +0000

    Switching to using docker image

--- API/inferenceFunction/app.py
+++ API/inferenceFunction/app.py
@@ -1,16 +1,25 @@
-import sys
+import boto3, base64, cv2
+from urllib.parse import unquote
 # import tensorflow as tf
 
 def lambda_handler(event, context):
     try:
         body = event["body"]
     except Exception as e:
-        print("Could note read body")
+        print("Invalid request body.")
         return {
             "statusCode": 400,
-            "body": "Invalid request body"
+            "body": "Invalid request body."
         }
-    print(body)
+    body = unquote(body) # Url decode body
+    body = base64.b64decode(body)
+    f = open("tmp/tmp.jpg", "wb")
+    f.write(body)
+    f.close()
+    img = cv2.imread("tmp/tmp.jpg", 0)
+    # s3 = boto3.resource('s3')
+    # object = s3.Object('tc-emotion-recognition-models', 'test.jpg')
+    # object.put(Body=body)
     return {
         "statusCode": 200,
         "body": "Request successful"
