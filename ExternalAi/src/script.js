6f08b6c9dc2b1afca9a067a19b258edafdd78130
TWCurry <thomas.w.curry@accenture.com>
2021-01-10 22:11:11 +0000

    Added webcam permissions checking code in external ai frontend

--- ExternalAi/src/script.js
+++ ExternalAi/src/script.js
@@ -3,21 +3,41 @@ var apiUrl = "http://35.190.172.118:5000"
 $( document ).ready(function() {
     console.log("DOM Loaded.");
     Webcam.set({
-        width: 220,
-        height: 190,
+        width: 60,
+        height: 45,
         image_format: 'jpeg',
         jpeg_quality: 100
     });
     Webcam.attach('#camera');
+
+    // Detect whether the user has given permission (asks again, but unlikely the user will deny the second time)
+    navigator.getMedia = ( navigator.getUserMedia ||
+        navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia ||
+        navigator.msGetUserMedia);
+
+    navigator.getMedia({video: true}, function() {
+        // Take first image
+        takeSnapShot()
+        // Trigger function every 5 seconds
+        setInterval(takeSnapShot, 5000)
+    }, function() {
+        console.log("Webcam not available.");
+    });
     
-    takeSnapShot = function () {
+});
+
+function takeSnapShot() {
+    console.log("Sending image...")
+    try {
         Webcam.snap(function (data) {
             document.getElementById('output').innerHTML = '<img src="' + data + '" width="70px" height="50px" />';
             params = {"imageData": data};
-            console.log(apiUrl+"/uploadImage")
             $.post(apiUrl+"/uploadImage", params, function(resp) {
                 console.log(resp);
             })
         });
-    }
-});
+      } catch (error) {
+          console.log("Error:"+error);
+      }
+}
