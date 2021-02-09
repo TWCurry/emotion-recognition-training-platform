7be7e7fb33592784b564d1d1795221830f0b1b4c
TWCurry <thomas.w.curry@accenture.com>
2021-02-09 10:33:55 +0000

    Added log viewing to autotraining bash script

--- AutoTraining/deployment/run.sh
+++ AutoTraining/deployment/run.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 # Deploy container
+echo "============== Deploying Container... =============="
 sudo docker-compose up -d
 
+# View logs
+echo "============== Container Logs =============="
+sudo docker logs -f deployment_model-trainer_1
+
 # Delete container once it's finished
+echo "============== Deleting Container... =============="
 sudo docker stop deployment_model-trainer_1
 sudo docker rm deployment_model-trainer_1
